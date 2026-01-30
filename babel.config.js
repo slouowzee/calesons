51928788b92f8ad59dbee0098ba09d40fcf5c590
slouowzee <gael.pilet1@gmail.com>
2026-01-30 16:43:15 +0100

    chores: added react-native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,13 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
       // [
       //   '@tamagui/babel-plugin',
       //   {
